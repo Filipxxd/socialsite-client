32fafe23c10a3510054f3574c6e02af0b43a25d5
filip.chromecka <filip.chromecka@outlook.com>
2024-11-20 17:22:50 +0100

    Add Post API placeholders

--- src/pages/posts/api.ts
+++ src/pages/posts/api.ts
@@ -15,8 +15,50 @@ export interface CreatePostRequest {
 
 export const createPost = async (values: CreatePostRequest) => {
     // TODO: Use AuthAxiosInstance
-    return await axios.post(`${API_BASE_URL}/posts/create`, {
+    return await axios.post(`${API_BASE_URL}/posts`, {
         Content: values.content,
+        Visibility: values.visibility,
         Images: values.images
     });
+}
+
+export interface UpdatePostRequest extends CreatePostRequest {
+    postId: number;
+}
+
+export const updatePost = async (values: UpdatePostRequest) => {
+    // TODO: Use AuthAxiosInstance
+    return await axios.put(`${API_BASE_URL}/posts/${values.postId}`, {
+        Content: values.content,
+        Visibility: values.visibility,
+        Images: values.images
+    });
+}
+
+export const deletePost = async (postId: number) => {
+    // TODO: Use AuthAxiosInstance
+    return await axios.delete(`${API_BASE_URL}/posts`, {
+        params: { postId },
+    });
+}
+
+export interface CommentResponse{
+    commentId: number;
+    content: string;
+    userFullName: string;
+    dateCreated: Date;
+}
+
+export interface PostResponse{
+    postId: number;
+    content: string;
+    images: File[];
+    dateCreated: Date;
+    userFullName: string;
+    comments: CommentResponse[];
+}
+
+export const getAllMainPagePosts = async () => {
+    // TODO: Use AuthAxiosInstance
+    return await axios.get<PostResponse[]>(`${API_BASE_URL}/posts`);
 }
