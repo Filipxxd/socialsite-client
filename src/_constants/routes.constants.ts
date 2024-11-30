b43ed72d93ba4a8bc47b4c1f2013d46b28a0dcec
Filip Chromečka <filip.chromecka@outlook.com>
2024-12-01 00:20:46 +0100

    Filetype change

--- src/_constants/routes.constants.ts
+++ src/_constants/routes.constants.ts
@@ -4,4 +4,5 @@ export const RegisterRoute = "/register";
 export const ChatsRoute = "/chats";
 export const PostsRoute = "/posts";
 export const FriendsRoute = "/friends";
-export const MyProfileRoute = "/profile";
+export const MyProfileRoute = "/my-profile";
+export const UserProfileRoute = "/profile";
