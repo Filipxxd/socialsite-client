944198ed7389216c30af4764c1bcd22f39394346
Filip Chromečka <filip.chromecka@outlook.com>
2025-01-01 13:16:49 +0100

    Add helpers

---
+++ src/_helpers/date.helper.ts
@@ -0,0 +1,13 @@
+﻿export function formatDate(date: string | Date): string {
+  const dateObj = new Date(date);
+
+  const padToTwoDigits = (num: number) => num.toString().padStart(2, '0');
+
+  const day = padToTwoDigits(dateObj.getDate());
+  const month = padToTwoDigits(dateObj.getMonth() + 1);
+  const year = dateObj.getFullYear();
+  const hours = padToTwoDigits(dateObj.getHours());
+  const minutes = padToTwoDigits(dateObj.getMinutes());
+
+  return `${day}. ${month}. ${year} ${hours}:${minutes}`;
+}
---
+++ src/_helpers/file.helper.ts
@@ -0,0 +1,12 @@
+﻿export const convertFileToBase64 = (file: File): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+};
+
+export const convertFilesToBase64 = (files: File[]): Promise<string[]> => {
+  return Promise.all(files.map((file) => convertFileToBase64(file)));
+};
